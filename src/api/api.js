dce3dbe41b34dd1fdd60ef8464e515f9bd86bd2d
K-Basket <koshykkv@gmail.com>
2023-09-29 22:23:57 +0300

    Update api.js

--- src/api/api.js
+++ src/api/api.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 const BASE_URL = 'https://65005a6318c34dee0cd4c52c.mockapi.io/';
 
 export const getAllCars = async () => {
-  const { data } = await axios.get(`${BASE_URL}advert`);
+  const { data } = await axios.get(`${BASE_URL}adverts`);
 
   return data;
 };
